fef09b2bc8edf2f60fbd1e399a0f103137759c07
LinHanLi <h1nlee@163.com>
2021-08-19 09:42:06 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+name='yaya_metal',
+version='0.1',
+author='LH. Li',
+url='#',
+description='Package_for_Computational_Metallurgy',
+license='#',
+packages=find_packages(),
+install_requires=['numpy', 'scipy', 'sympy',  'statsmodels', 'ase', 'ovito' ,'myalloy-pkg'],
+)
