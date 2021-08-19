eaaf80756961b4963fdcb6276e2554f543a6d411
LinHanLi <h1nlee@163.com>
2021-08-19 09:58:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-setup(
-name='yaya_metal',
-version='0.1',
-author='LH. Li',
-url='#',
-description='Package_for_metal_atoms',
-license='#',
-packages=find_packages(),
-install_requires=['numpy', 'scipy', 'sympy',  'statsmodels', 'ase', 'ovito' ,'myalloy-pkg'],
-)
+here = path.abspath(path.dirname(__file__))
+
+
+setup(name='yaya_metal',
+      version='0.1',
+
+      description='Package for metellurgy calculations',
+
+      # URL
+      url="#",
+
+      # Author
+      author="LH.Li",
+      author_email='lh98lee@zju.edu.cn',
+
+      license='#',
+
+      packages=find_packages(),
+      )
+
+
+# python3 setup.py install --u
